0cbde17a8eed533adaefe3663c2e29f6f0966647
Kamil <prospero.apps@gmail.com>
2022-09-14 14:36:45 +0200

    Clean up the code

--- src/components/Cards.js
+++ src/components/Cards.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import '../styles.css';
 import Card from './Card';
 
@@ -18,13 +18,11 @@ import flower12 from '../images/flower12.png';
 const Cards = ({ onClickImage }) => {
   const [images, setImages] = useState(flowerImages);
 
-  // const handleOnClickImage = (e) => {
-  //   console.log(e.target.picked)
-  //   const newFlowerImages = [...images];
-  //   shuffle(newFlowerImages);
-  //   setImages(newFlowerImages);    
-  //   onClickImage(e);
-  // }
+  useEffect(() => {
+    const newFlowerImages = [...images];
+    shuffle(newFlowerImages);
+    setImages(newFlowerImages);
+  }, []);
 
   const handleOnClickImage = (scoring) => {
     const newFlowerImages = [...images];
@@ -45,7 +43,7 @@ const Cards = ({ onClickImage }) => {
     <Card 
       key={flower.id}
       imageSource={flower.source}
-      alreadyClicked={flower.picked}
+      alreadyClicked={false}
       onClickImage={handleOnClickImage}
     />)
 
@@ -59,51 +57,39 @@ const Cards = ({ onClickImage }) => {
 const flowerImages = [{
   id: 1,
   source: flower1,
-  picked: false,
 }, {
   id: 2,
   source: flower2,
-  picked: false,
 }, {
   id: 3,
   source: flower3,
-  picked: false,
 }, {
   id: 4,
   source: flower4,
-  picked: false,
 }, {
   id: 5,
   source: flower5,
-  picked: false,
 }, {
   id: 6,
   source: flower6,
-  picked: false,
 }, {
   id: 7,
   source: flower7,
-  picked: false,
 }, {
   id: 8,
   source: flower8,
-  picked: false,
 }, {
   id: 9,
   source: flower9,
-  picked: false,
 }, {
   id: 10,
   source: flower10,
-  picked: false,
 }, {
   id: 11,
   source: flower11,
-  picked: false,
 }, {
   id: 12,
   source: flower12,
-  picked: false,
 },]
 
 export default Cards;
